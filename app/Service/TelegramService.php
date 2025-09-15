ebf10e7ad806965882a65284be58905d0779e29d
io <io@admin.com>
2025-09-16 01:37:30 +0800

    123

--- app/Service/TelegramService.php
+++ app/Service/TelegramService.php
@@ -10,7 +10,7 @@ class TelegramService
     public static function sendMessage(string $text)
     {
         Request::initialize(new Telegram('7620073499:AAEMY6RTvtqSyBf8U4OPpJ4e68jgBYX5JEo', 'MyNoticeBot'));
-        
+        // https://api.telegram.org/bot token/getUpdates
         Request::sendMessage([
             'chat_id' => 6110160562,
             'text' => $text,
